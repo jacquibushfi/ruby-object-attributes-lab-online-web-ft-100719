6d68fe8ac4f6d10b955c3e8d7e55bc4ea7926d36
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-14 13:37:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,9 @@
- def initialize(name)
-    @name = name
-  end
- 
-  def name  # getter or reader method used to create the instance variable
+class Person
+  def name 
     @name
   end
  
-  def name=(new_name) #setter method defined with the "="
+  def name=(new_name)
     @name = new_name
   end
  
